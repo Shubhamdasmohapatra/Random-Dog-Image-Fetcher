8c92eb11deedcaf5de01be338f3d651b70302321
Shubham Das Mohapatra <51811720+Shubhamdasmohapatra@users.noreply.github.com>
2021-01-12 12:08:42 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+function fetchRandomDogImage(){
+
+	var xhrRequest = new XMLHttpRequest();
+
+	xhrRequest.onload = function(){
+		console.log(xhrRequest.response);
+		var responseJSON = JSON.parse(xhrRequest.response);
+		var imageURL = responseJSON.message;
+		$('#dog-image').attr('src', imageURL);
+	};
+
+	xhrRequest.open('get','https://dog.ceo/api/breeds/image/random');
+	xhrRequest.send();
+}
+
+
+
+$('#fetch-dog-image-button').click(fetchRandomDogImage);
